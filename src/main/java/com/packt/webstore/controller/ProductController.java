d972ccb0ed74f7ea93ce2f2618d1c84874b81b8c
Spawnshik <spawnshik@gmail.com>
2017-02-02 16:00:24 +0200

    correct

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -1,23 +1,19 @@
 package com.packt.webstore.controller;
 
-import com.packt.webstore.domain.Product;
+import com.packt.webstore.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.math.BigDecimal;
 
 @Controller
 public class ProductController {
     @RequestMapping("/products")
     public String list(Model model) {
-        Product iphone = new Product("P1234", "iPhone 5s", new BigDecimal(500));
-        iphone.setDescription("Apple iPhone 5s smartphone with 4.00-" +
-                "inch 640x1136 display and 8-megapixel rear camera");
-        iphone.setCategory("Smart Phone");
-        iphone.setManufacturer("Apple");
-        iphone.setUnitsInStock(1000);
-        model.addAttribute("product", iphone);
+        model.addAttribute("products", productRepository.getAllProducts());
         return "products";
     }
+
+    @Autowired
+    private ProductRepository productRepository;
 }
